c8b7d513a9d0a4525bf7c88e71bd90dfb4b0b02c
zhampel <17304045+zhampel@users.noreply.github.com>
2019-04-15 17:31:22 -0700

    Adds init script and supporting files to start building

---
+++ clusgan/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+
+from .__version__ import __version__
+
+from .utils import *
+from .train import *
+from .models import *
+from .definitions import *
---
+++ clusgan/definitions.py
@@ -0,0 +1,10 @@
+import os
+
+# Local directory of CypherCat API
+CLUSGAN_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Local directory containing entire repo
+REPO_DIR = os.path.split(CLUSGAN_DIR)[0]
+
+# Local directory for datasets
+DATASETS_DIR = os.path.join(REPO_DIR, 'datasets')
